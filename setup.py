872392c07da0e0cf08112b4bd684d17ceeb8ddcd
Brian West <brian@signalwire.com>
2025-04-07 23:58:35 +0000

    updat eversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="signalwire-pom",
-    version="0.1.2",
+    version="0.1.3",
     author="SignalWire",
     author_email="support@signalwire.com",
     description="Prompt Object Model - A structured data format for organizing and rendering LLM prompts",
--- signalwire_pom/__init__.py
+++ signalwire_pom/__init__.py
@@ -1,5 +1,5 @@
 from signalwire_pom.pom import PromptObjectModel, Section
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ["PromptObjectModel", "Section"] 
