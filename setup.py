f336264d2f65828c8e06f76df4c1f9bb9f07127a
Brian West <brian@signalwire.com>
2025-04-24 23:29:40 +0000

    bump to 2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="signalwire-pom",
-    version="0.2.3",
+    version="2.0",
     author="SignalWire",
     author_email="support@signalwire.com",
     description="Prompt Object Model - A structured data format for organizing and rendering LLM prompts",
--- signalwire/__init__.py
+++ signalwire/__init__.py
@@ -1,5 +1,5 @@
 from signalwire.pom import PromptObjectModel, Section
 
-__version__ = "0.2.3"
+__version__ = "2.0"
 
 __all__ = ["PromptObjectModel", "Section"] 
